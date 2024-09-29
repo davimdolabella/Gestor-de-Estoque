fe6691fdee10177681a12eddcdc925fd2806e805
davimdolabella <davimdolabella03@gmail.com>
2024-09-29 10:31:12 -0300

    testando router 5º vez

--- src/router.jsx
+++ src/router.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
                 element: <CriarItem/>
             },
             {
-                path:'itens/editarItem/:itemId',
+                path:'itens/:itemId/editarItem',
                 element:<EditarItem/>
             }
         ]
